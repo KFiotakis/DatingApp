0c17121464cc92520925252d1b0b8d5f29911588
KFiotakis <fiotiskostas@gmail.com>
2022-11-07 11:57:27 +0200

    Delivering Data to Client

--- API/myApp/src/app/app.component.ts
+++ API/myApp/src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  
   title = 'myApp';
+  users: any;
+  private URL = "https://localhost:5001/api/users";
+
+  constructor(private http: HttpClient) { }
+
+  getUsers():void {
+    this.http.get(this.URL).subscribe(
+      {  next: response => { this.users = response},
+        error: e => { console.log(e); },
+        complete: ()=> console.log(this.users)
+      }
+    )
+  }
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
 }
